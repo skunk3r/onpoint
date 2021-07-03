1ac02f9aa1550010613a684647716117d3a1a0a3
skunk3r <ganek13@yandex.ru>
2021-07-03 22:20:04 +0300

    refactor: Переписать header и добавить объекты статичного фона

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,17 @@ export default function Header({elem, swipe}) {
 	return (
 		<header>
 			<img 
-				src='img/home.png' 
-				alt='home' 
-				useMap='#navigation' 
+				src='img/home-button.png' 
+				alt='home'
+				className='home-button'
+				onPointerDown={(e) => e.preventDefault()} 
+				onClick={() => swipe(elem.current, 'right', true)} />
+			<img 
+				src='img/home-line.png' 
+				alt='line'
+				className='home-line'
 				onPointerDown={(e) => e.preventDefault()} />
-			<map name="navigation">
-				<area 
-					onClick={() => swipe(elem.current, 'right', true)} 
-					shape='rect' coords='0,0, 21,27' 
-					nohref='true' alt='home' />
-			</map>
+			<p>Project</p>
 			<button onClick={() => swipe(elem.current, 'right')}>&#8592;</button>
 			<button onClick={() => swipe(elem.current, 'left')}>&#8594;</button>
 		</header>
